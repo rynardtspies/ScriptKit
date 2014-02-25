f882502f61f63a13fcec02f618ada70b9e103288
Rynardt Spies <rynardt.spies@virtualvcp.com>
2014-02-25 16:49:46 +0000

    Added simple error handling to connect-viserver

--- VMware PowerCLI/Configure/set_vmhost_shell_timeout.ps1
+++ VMware PowerCLI/Configure/set_vmhost_shell_timeout.ps1
@@ -1,6 +1,6 @@
 #Author: Rynardt Spies
 #Author Contact: rynardt.spies@virtualvcp.com / www.virtualvcp.com / @rynardtspies
-#Version: v1.00.00
+#Version: v1.00.01
 #Updated: February 2014
 #Set the ShellTimeOut Setting for all hosts in the specified cluster(s) and restarts
 # the ESXiShell Service. Changes require the SSHShell TSM service to be restarted
@@ -21,8 +21,13 @@ $shellTimeOutValue =  1200
 
 #Clear the console screen
 Clear Screen
-write-Output "Connecting to vSphere Environment $vcenter"Connect-VIServer $vcenter
 
+write-Output "Connecting to vSphere Environment $vcenter"#Try to connect to $vcenter. If not, fail gracefully with a message
+if (!($ConnectionResult = Connect-VIServer $vcenter -ErrorAction SilentlyContinue)){
+	Write-Output "Could not connect to $vcenter. Check server address."
+	break
+	}
+Write-Output "Successfully connected to: $ConnectionResult"
 foreach ($cluster in $clusters){
 	#Test if the current $cluster exists in the environment. If it doesn't continue to next $cluster
 	if(!(Get-Cluster $cluster -ErrorAction SilentlyContinue)){
