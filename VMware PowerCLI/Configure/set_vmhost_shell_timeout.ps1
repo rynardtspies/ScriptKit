1cc1d11efd3b4697e595af33f7529333b23a18e5
Rynardt Spies <rynardt.spies@virtualvcp.com>
2014-02-25 14:17:37 +0000

    Fixed confirmActions Var

--- VMware PowerCLI/Configure/set_vmhost_shell_timeout.ps1
+++ VMware PowerCLI/Configure/set_vmhost_shell_timeout.ps1
@@ -1,6 +1,6 @@
 #Author: Rynardt Spies
 #Author Contact: rynardt.spies@virtualvcp.com / www.virtualvcp.com / @rynardtspies
-#Version: v0.01.00
+#Version: v0.02.00
 #Updated: February 2014
 #Set the ShellTimeOut Setting for all hosts in the specified cluster(s)
 #==================================================================================
@@ -11,7 +11,7 @@ $vcenter = "vcenter"
 #List clusters by name, separate multiple clusters by comas.
 $clusters = @("cluster01","cluster02")
 #Confirm actions ($true or $false)
-$conrimActions = $false
+$confirmActions = $false
 #Change the Time out to the value specified here. (Default=1200, Disabled=0)
 $shellTimeOutValue =  600
 
@@ -22,9 +22,9 @@ write-Output "Connecting to vSphere Environment $vcenter"Connect-VIServer $vcen
 foreach ($cluster in $clusters){
 	foreach ($esxhost in (Get-VMHost -Location $cluster)){
 		$currentShellTimeOut = Get-AdvancedSetting -Entity $esxhost -Name UserVars.ESXiShellTimeOut
-		$newShellTimeOut = $currentShellTimeOut | Set-AdvancedSetting -Value $shellTimeOutValue -confirm:$conrimActions
+		$newShellTimeOut = $currentShellTimeOut | Set-AdvancedSetting -Value $shellTimeOutValue -confirm:$confirmActions
 		Write-Output "$esxhost : Changed setting: $currentShellTimeOut to $newShellTimeOut"
 	}
 }
 Write-Output "Disconnecting from vSphere Environment $vcenter"
-Disconnect-VIserver $vcenter -confirm:$conrimActions
+Disconnect-VIserver $vcenter -confirm:$confirmActions
