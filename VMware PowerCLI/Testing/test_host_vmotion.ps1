3b91d1580573d0f2276cd2aa992dcf75a5a1ddad
Rynardt Spies <rynardt.spies@virtualvcp.com>
2014-03-13 00:34:37 +0000

    added connectivity tests after each vMotion migration

--- VMware PowerCLI/Testing/test_host_vmotion.ps1
+++ VMware PowerCLI/Testing/test_host_vmotion.ps1
@@ -1,6 +1,6 @@
 #Author: Rynardt Spies
 #Author Contact: rynardt.spies@virtualvcp.com / www.virtualvcp.com / @rynardtspies
-#Version: v1.00.00
+#Version: v1.00.01
 #Updated: March 2014
 #Test vMotion on all hosts in specified cluster by migrating a test VM between hosts at
 # a set interval
@@ -15,6 +15,10 @@ $testvm = "TestVM"
 $cluster = "ClusterName"
 #Enter the interval in seconds between migrations
 $interval = 60
+#Enter the number of packets to ping the testvm with
+$pingtestpacketcount = 10
+#Specify the resport file path and filename 
+$reportfile = "C:\Temp\test_host_vmotion-report.csv"
 
 Clear Screen
 #Connect to vcenter server
@@ -25,6 +29,7 @@ if(!($ConnectionResult = Connect-VIServer $vcenter -ErrorAction SilentlyContinue
 }
 #If the script got this far, then we should be connected
 Write-Output "Connected to $ConnectionResult"
+$report = @()
 
 if (!($actCluster = Get-Cluster $cluster -ErrorAction SilentlyContinue)){
 	Write-Output "The object $cluster was not found on $ConnectionResult. Exiting..."
@@ -49,19 +54,51 @@ if (!($migVM = Get-VM $testvm -ErrorAction SilentlyContinue)){
 	Write-Output "Disconnected from $ConnectionResult"
 	break
 }
+$TestVMIP = $migVM.ExtensionData.Summary.Guest.IpAddress
+Write-Output "All network connectivity testing to $migVM will be carried out using IP $TestVMIP"
 
 #Ensure that the test VM is on the first host in the cluster, if not migrate it to the host
-if (!($migVM.VMHost -eq  $esxhosts[0].Name)){
+if (!($migVM.VMHost -eq  $esxhosts[0])){
 	Write-Output "Moving $migVM to first host in cluster $cluster"
-	Move-VM $testvm -Destination $esxhosts[0]
+	Move-VM $migVM -Destination $esxhosts[0] | select Name,PowerState,VMHost
+	Write-Output "Testing network connectivity to $migVM on $TestVMIP..."
+	if ($PingResult = Test-Connection $TestVMIP -Count $pingtestpacketcount -Quiet){
+		Write-Output "Network connectivity test to $migVM successful. "
+	} else {
+		write-output "Network connectivity test to $migVM failed..."
+	}
+	$row = "" | select VMName, VMHostName, TestedIP, PingTestPass
+	$row.VMName = $migVM.Name
+	$row.VMHostName = $migVM.VMHost
+	$row.TestedIP = $TestVMIP
+	$row.PingTestPass = $PingResult
+	$report += $row
 }
+	
 for ($i = 1; $i -le ($esxhosts.count -1); $i++){
+	#Refresh $migVM details to current residing host
+	$migVM = Get-VM $testvm
 	$nextVMHost = $esxhosts[$i]
 	Write-Output "Waiting $interval seconds before next migration..."
 	Start-Sleep -s $interval
 	Write-Output "Migrating $migVM to $nextVMHost..."
-	Move-VM $migVm -Destination $nextVMHost
+	Move-VM $migVM -Destination $nextVMHost |select Name,PowerState,VMHost
+	Write-Output "Testing network connectivity to $migVM on $TestVMIP..."
+	if ($PingResult = Test-Connection $TestVMIP -Count $pingtestpacketcount -Quiet){
+		Write-Output "Network connectivity test to $migVM successful. "
+	} else {
+		write-output "Network connectivity test to $migVM failed..."
+	}
+	$row = "" | select VMName, VMHostName, TestedIP, PingTestPass
+	$row.VMName = $migVM.Name
+	$row.VMHostName = $migVM.VMHost
+	$row.TestedIP = $TestVMIP
+	$row.PingTestPass = $PingResult
+	$report += $row
 }
+
+Write-Output "Writing results report to $reportfile"
+$report | export-csv $reportfile -NoTypeInformation
 
 Write-Output "Disconnecting form $ConnectionResult"
 Disconnect-VIServer $ConnectionResult -confirm:$false
